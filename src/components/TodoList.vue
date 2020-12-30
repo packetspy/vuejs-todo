5ee64b25c720fd55ef21d1cf174681acdead7931
Anderson Tomazeto <packetspy@gmail.com>
2020-12-30 13:04:15 -0300

    Fix alignment to center

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <sui-card class="card" v-for="todo in todoList" v-bind:key="todo.id">
+    <sui-card class="card centered" v-for="todo in todoList" v-bind:key="todo.id">
       <sui-card-content>
         <sui-card-header>
           {{todo.title}}
         </sui-card-header>
       </sui-card-content>
       <sui-card-content>
+        <p>{{todo.project}}</p>
+      </sui-card-content>
+      <sui-card-content extra>
+        <sui-button>Done</sui-button>
       </sui-card-content>
     </sui-card>
-
-    1 - {{todoList}}
-
   </div>
 </template>
 
